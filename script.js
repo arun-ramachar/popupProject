a5873de6e489e08ea581829bd71a530f6cd0dc83
ArunRamachar <arunramachar@gmail.com>
2021-11-02 12:13:47 +0530

    all changes done

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const popups = document.querySelectorAll(".popup");
+const popup= document.querySelector(".output")
+const popupMessage= document.querySelector(".message")
+const popupClose= document.querySelector(".close")
 
+  popupClose.addEventListener("click",function(){
+    popup.classList.add("hide");
+  })
 for(let x=0;x<popups.length;x++){
 
-    console.log(popups[x]);
+    //console.log(popups[x]);
     popups[x].addEventListener("click",function(){
       
           let outputText=this.getAttribute("data-message");
-          console.log(outputText);
+          message(outputText);
 
     })
+}
+
+function message(output){
+    popup.classList.remove("hide")
+    popupMessage.innerHTML= output;
 }
 
